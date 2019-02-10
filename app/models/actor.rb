354c4262cac6d85ef52d06be6970017f6284f228
stomg7969 <stomg7969@gmail.com>
2019-02-10 13:22:17 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, :through => :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.map { |char| "#{char.name} - #{char.show.name}" }
+    #char.show.name => self.shows.name will not work because self.shows returns an array.
+  end
+
 end
