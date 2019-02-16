64305f3fcba4508efc5c9d1a63251b35db44077e
github username <github email address>
2019-02-16 17:26:56 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
-end
+
+has_many :characters
+has_many :shows, through: :characters
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    return "#{self.characters.pluck(:name)[0]} - #{self.shows.pluck(:name)[0]}"
+  end
+
+end # end of class
